06ed80c46b901e0e899752bd1863a4e07c571aed
Afsaruddin12133 <afsaruddin12133@gmail.com>
2022-04-05 21:37:41 +0600

    added q&a section

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1 className='text-center text-2xl mt-10'>From About</h1>
+        <div className='p-10 m-3'>
+            <h1 className='text-center text-4xl mt-10 text-[#F2A65E]'>About PizzaTeca !! </h1>
+            <p className='text-xl mt-6 mb-50'>Chicken sausage Best Pizza Take from Pizzateca.This is the best pizza delivery recode in pizzateca.Pizzateca service is very awesome. Americans eat approximately 100 acres of pizza each day, or 350 slices per second. They are take from Pizzateca. It is a world wide pizza hut.94% of Americans eat pizza regularly from pizzateca.You Have take super Offer from pizzateca.pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese, and many other ingredients, baked quickly—usually, in a commercial setting, using a wood-fired oven heated to a very high temperature—and served hot </p>
         </div>
     );
 };
--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-       <div className='m-8 md:flex md:justify-around '>
+     <div>
+           <div className='m-8 md:flex md:justify-around '>
         <div className='border w-[500px] p-3'>
         <h1 className='text-4xl text-bold mt-10 mb-5'>Q: What is Context Api?</h1>
         <h3 className='text-xl'>Ans: If you want to pass some data in your react application without props you can pass your data globaly in you and child component using React Context Api. In Short Context Api is a way for a React app to effectively produce global variables that can be passed around.Fast of all you can create createContext Hooks in your apps. then you can wrape in your aria conponent it another hook .Then you pass your data anywhere.</h3>
@@ -12,6 +13,24 @@ const Blogs = () => {
         <h3 className='text-xl'>Ans: Semantic tag denote by clear meaning devloper and there team.Semantic tag look like good. It is a usderstanding and mening full element. For Example form , table , and article etc.this all tag are semantic tag.</h3>
         </div>
        </div>
+           <div className='border w-[500px] p-3 mx-[110px]'>
+           <h1 className='text-4xl text-bold mt-10 mb-5'>Q: Different between inline element and inline block element?</h1>
+           <h3 className='text-xl'>Inline Element : <ul>
+               <li>They don't consume the full width</li>
+               <li>They don't start in a new line</li>
+               <li>They don't have have top and bottom margins</li>
+               <li>Example : span,small,a ,br,img etc</li>
+               </ul></h3>
+           <h3 className='text-xl'>Block Element : <ul>
+               <li>They consume the full width</li>
+               <li>They always start in a new line</li>
+               <li>They always have have top and bottom margins</li>
+               <li>Example : h1-h6,div,p etc</li>
+               
+               </ul></h3>
+   
+           </div>
+     </div>
     );
 };
 
